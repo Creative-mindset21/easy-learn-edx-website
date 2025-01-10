4c35b7175a6ba7f90a1d7e8469f81503bccde817
Creative Mindset <tobiawotoye03@gmail.com>
2025-01-10 03:44:26 +0000

    updated the header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,10 +9,23 @@ const Header = () => {
       <header id="container" className={css.header}>
         <div className={css.logo}>
           <NavLink to="/">LearnEasy</NavLink>
-          <NavLink to="register" className="btn">
-            Get Started Today
-          </NavLink>
         </div>
+
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="#about">Benefits</NavLink>
+            </li>
+            <li>
+              <NavLink to="#testimonies">Testimonials</NavLink>
+            </li>
+            <li>
+              <NavLink to="register" className="btn">
+                Get Started Today
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </header>
 
       <Outlet />
